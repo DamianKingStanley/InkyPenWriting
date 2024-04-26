84139e9e46983c098a960326053a7b9eedc4ae79
King Damian <damianstanley76@gmail.com>
2024-04-26 09:59:14 +0100

    first commit

---
+++ frontend/src/pages/Sponsorship/Sponsorship.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Sponsorship.css";
+import Navbar from "../../component/Navbar/Navbar";
+import Navibar from "../../component/Navibar/Navibar";
+
+const Sponsorship = () => {
+  return (
+    <div className="SponsorhipBody">
+      <Navbar />
+      <Navibar />
+      <section></section>
+    </div>
+  );
+};
+
+export default Sponsorship;
---
+++ server/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import postModel from "./models/post.js";
+import userModel from "./models/user.js";
+import postRoute from "./routes/postRoute.js";
+import userRoute from "./routes/userRoute.js";
+import commentRoute from "./routes/commentRoute.js";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+// middlewares
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const MONGODB_URL = "mongodb://localhost:27017/Inkypen_db";
+// const MONGODB_URL =
+//   "mongodb+srv://webmanagement:webmanagement@damian.yv76yyk.mongodb.net/InkypenWebsite_db";
+
+const PORT = 5000;
+await mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect`);
+  });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome" });
+});
+
+app.use("/", commentRoute);
+app.use("/", postRoute);
+app.use("/", userRoute);
